7def187013d0aad8963267f7f6e26c3e8832b7a1
Arthur Vieira Ribeiro <arthurribeiro@sdc-sc.intranet.sdc.sc.gov.br>
2025-02-24 14:53:57 -0300

    feat: favicon adicionado + roteamento para o gerador de assinatura caso o usuario tente acessar outra rota

--- servidor-node/server.js
+++ servidor-node/server.js
@@ -7,6 +7,10 @@ const PORT = 80; // Porta de acesso na rede
 // Servir arquivos estáticos da pasta "public"
 app.use('/gerador-de-assinatura', express.static(path.join(__dirname, 'public')));
 
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
 // Iniciar servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando! Acesse: http://${getLocalIP()}/gerador-de-assinatura`);
